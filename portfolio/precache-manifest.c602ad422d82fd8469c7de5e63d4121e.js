2b17d5f183dd77e8ed796372ab125f0d12e2c7f6
ratanajaya <ratanajayaa@gmail.com>
2021-01-05 14:32:19 +0700

    prebuilt wk app

---
+++ portfolio/precache-manifest.c602ad422d82fd8469c7de5e63d4121e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588e10b556b677343e479ce71058881d",
+    "url": "/portfolio/index.html/index.html"
+  },
+  {
+    "revision": "e9258c4642db682ad502",
+    "url": "/portfolio/index.html/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "4e48a79469da89ecad95",
+    "url": "/portfolio/index.html/static/css/main.76d5c488.chunk.css"
+  },
+  {
+    "revision": "e9258c4642db682ad502",
+    "url": "/portfolio/index.html/static/js/2.04920ac8.chunk.js"
+  },
+  {
+    "revision": "e9c29f12aa988bb592d2d421b5d2a1d8",
+    "url": "/portfolio/index.html/static/js/2.04920ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e48a79469da89ecad95",
+    "url": "/portfolio/index.html/static/js/main.f48fe1d8.chunk.js"
+  },
+  {
+    "revision": "638342af3a7c4f7a9dc9",
+    "url": "/portfolio/index.html/static/js/runtime-main.79601754.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/portfolio/index.html/static/media/loading.d47e58c7.gif"
+  }
+]);
