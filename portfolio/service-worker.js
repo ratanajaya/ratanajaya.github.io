9c0e2b28d7d42e52838268cc9ab5e6cca613fe0b
ratanajaya <ratanajayaa@gmail.com>
2021-01-14 09:57:43 +0700

    general improvement

--- portfolio/service-worker.js
+++ portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a5e3c98879651b5161c63d912936a8a4.js"
+  "/portfolio/precache-manifest.3111621414f52753af72ccd5ec8c8398.js"
 );
 
 self.addEventListener('message', (event) => {
