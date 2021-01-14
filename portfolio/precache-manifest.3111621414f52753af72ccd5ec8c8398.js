f07513ff81de5d11866d5ce72294e427301dcb30
fakewars <fakewarszero@gmail.com>
2021-01-14 09:57:43 +0700

    general improvement

---
+++ portfolio/precache-manifest.3111621414f52753af72ccd5ec8c8398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6848d8e4b31d9170a90f318375e51a4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e60f55864815220b080c",
+    "url": "/portfolio/static/css/2.888a8430.chunk.css"
+  },
+  {
+    "revision": "21489dd7deae483f840e",
+    "url": "/portfolio/static/css/main.dbaf191b.chunk.css"
+  },
+  {
+    "revision": "e60f55864815220b080c",
+    "url": "/portfolio/static/js/2.302d9fc5.chunk.js"
+  },
+  {
+    "revision": "e9c29f12aa988bb592d2d421b5d2a1d8",
+    "url": "/portfolio/static/js/2.302d9fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21489dd7deae483f840e",
+    "url": "/portfolio/static/js/main.abe017cc.chunk.js"
+  },
+  {
+    "revision": "fe3022f289cd97937264",
+    "url": "/portfolio/static/js/runtime-main.8532b210.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/portfolio/static/media/loading.d47e58c7.gif"
+  }
+]);
