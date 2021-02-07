4e08d28ab33a5ba27f89d98c2582232bafdffe18
ratanajaya <ratanajayaa@gmail.com>
2021-02-07 22:34:24 +0700

    move portfolio to homapage

---
+++ precache-manifest.3890a1901d7551df6eb14da2291e16e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034ec4d246b8bc0687963b6ab425fa4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c12843ab4dba2d6bc94",
+    "url": "/static/css/2.888a8430.chunk.css"
+  },
+  {
+    "revision": "7014994cddcd8d79e890",
+    "url": "/static/css/main.461873cd.chunk.css"
+  },
+  {
+    "revision": "1c12843ab4dba2d6bc94",
+    "url": "/static/js/2.bb609de5.chunk.js"
+  },
+  {
+    "revision": "e9c29f12aa988bb592d2d421b5d2a1d8",
+    "url": "/static/js/2.bb609de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7014994cddcd8d79e890",
+    "url": "/static/js/main.d53db4f3.chunk.js"
+  },
+  {
+    "revision": "f4425560d8bd2bcc0d7d",
+    "url": "/static/js/runtime-main.d0eb0cad.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/static/media/loading.d47e58c7.gif"
+  }
+]);
