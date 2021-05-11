ef584b7f7ccaf45f16bec5a497252cecc5fdd171
ratanajaya <ratanajayaa@gmail.com>
2021-05-11 21:35:14 +0700

    integrate template. wkr app with azure backend

---
+++ wkr/precache-manifest.261e2021ace6a211f1f523733f2dc197.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908d597504724a05f9a1735430595a43",
+    "url": "/wkr/index.html"
+  },
+  {
+    "revision": "99026fddacaf3d933e9b",
+    "url": "/wkr/static/css/2.888a8430.chunk.css"
+  },
+  {
+    "revision": "895b270981d9f5983133",
+    "url": "/wkr/static/css/main.994d215d.chunk.css"
+  },
+  {
+    "revision": "99026fddacaf3d933e9b",
+    "url": "/wkr/static/js/2.7f8901e9.chunk.js"
+  },
+  {
+    "revision": "26b0d02f309a323928a1d219329bed13",
+    "url": "/wkr/static/js/2.7f8901e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895b270981d9f5983133",
+    "url": "/wkr/static/js/main.ad103907.chunk.js"
+  },
+  {
+    "revision": "ee6d2fe0b9833d118002",
+    "url": "/wkr/static/js/runtime-main.2194abdb.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/wkr/static/media/loading.d47e58c7.gif"
+  }
+]);
