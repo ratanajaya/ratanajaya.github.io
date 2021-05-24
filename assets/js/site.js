6f58f763eb8c73a4a315568098377aa01d4ffbee
ratanajaya <ratanajayaa@gmail.com>
2021-05-25 02:28:27 +0700

    Big progress

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,17 +1,47 @@
 (function () {
-  document.getElementById("btnSubmit").addEventListener("click", () => {
-    var grecaptcha = document.getElementById('captcha');
+  const urlApi = 'https://localhost:44365/';
 
-    var response = grecaptcha.getResponse();
-    console.log('response', response);
+  document.getElementById("btnSubmit").addEventListener("click", (e) => {
+    e.preventDefault();
 
     const messageModel = {
       senderName: document.getElementById('senderName').value,
       content: document.getElementById('message').value,
-      recaptchaToken: ""
+      recaptchaToken: recaptchaToken
     };
 
     console.log('messageModel', messageModel);
+
+    fetch(urlApi + 'Portfolio/SendMessage', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageModel)
+    }).then(res => {
+      if (res.status === 200) {
+        swal("Success!", "Message Sent!", "success");
+      }
+      else if (res.status === 401) {
+        swal("error", "ReCaptcha verivication failed or expired", "error");
+      }
+      else {
+        swal("error", res.status.toString(), "error");
+        console.log('error', res);
+      }
+    })
+      .catch(function (err) {
+        swal("Fetch Error", JSON.stringify(err), "error");
+        console.log('Fetch Error', err);
+      });
   });
 
-})()
+})();
+
+var recaptchaToken = null;
+
+function recaptchaCallback(val) {
+  //console.log('recaptchaCallback', val);
+  recaptchaToken = val;
+}
